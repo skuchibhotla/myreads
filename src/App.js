cadf601b93f034c51cf979159373b3a15aaa9b68
Shashank Kuchibhotla <sk.kuchibhotla@gmail.com>
2024-03-07 01:27:11 -0700

    Fixing blank query issue on Searchh

--- src/App.js
+++ src/App.js
@@ -1,59 +1,73 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import * as BooksAPI from './BookAPI';
+import * as BooksAPI from "./BookAPI";
 import BookShelf from "./components/BookShelf";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Search from "./components/Search";
 
 const App = () => {
-
-  const [books, setBooks] = useState([]); // Empty array...  
+  const [books, setBooks] = useState([]); // Empty array...
+  // const [showSearchPage, setShowSearchpage] = useState(false);
 
   const shelves = [
-    {key:'currentlyReading', name:'Currently Reading'},
-    {key:'wantToRead', name:'Want to Read'},
-    {key:'read', name:'Read'},
+    { key: "currentlyReading", name: "Currently Reading" },
+    { key: "wantToRead", name: "Want to Read" },
+    { key: "read", name: "Read" },
   ];
 
   useEffect(() => {
-    BooksAPI.getAll()
-      .then((books) => {
-        setBooks(books);
-      })
+    BooksAPI.getAll().then((books) => {
+      setBooks(books);
+    });
   }, []);
 
   const moveBook = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-      .then(() => {
-        book.shelf = shelf;
+    BooksAPI.update(book, shelf).then(() => {
+      book.shelf = shelf;
 
-        // Setting the book with updated shelf info to books...
-        setBooks(prevBooks => {
-          const filteredBooks = prevBooks.filter(b => b.id !== book.id);
-          return [...filteredBooks, book];
-        })
-      });     
-  }
+      // Setting the book with updated shelf info to books...
+      setBooks((prevBooks) => {
+        const filteredBooks = prevBooks.filter((b) => b.id !== book.id);
+        return [...filteredBooks, book];
+      });
+    });
+  };
 
   return (
     <div className="app">
-      <Routes>
-        <Route exact path="/" element={
-          <div>
-            {shelves.map((shelf) => (
-              <BookShelf 
-                key={shelf.key}
-                title={shelf.name}
-                booksOnShelf={books.filter(book => book.shelf === shelf.key)}
-                moveBook={moveBook}
-              />
-            ))}
-          </div>
-        } />
-        <Route path="/search" element={<Search/>}/>
-      </Routes>
+      <div className="list-books">
+        <div className="list-books-content">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <div>
+                  <div className="list-books-title">
+                    <h1>MyReads</h1>
+                  </div>
+                  {shelves.map((shelf) => (
+                    <BookShelf
+                      key={shelf.key}
+                      title={shelf.name}
+                      booksOnShelf={books.filter(
+                        (book) => book.shelf === shelf.key
+                      )}
+                      moveBook={moveBook}
+                    />
+                  ))}
+                </div>
+              }
+            />
+            <Route path="/search" element={<Search moveBook={moveBook}/>} />
+          </Routes>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a Book</Link>
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import * as BooksAPI from '../BookAPI';
+import Book from "./Book";
 
-const Search = () => {
+const Search = ({moveBook}) => {
+    const [booksFetched, setBooksFetched] = useState([]);
+    const searchBook = (e) => {
+        // alert(e.target.value);
+        BooksAPI.search(e.target.value).then((books) => {
+            console.log(books);
+            if(books && books.length > 0)
+                setBooksFetched(books);
+            else
+                setBooksFetched([]);
+        }).catch((error) => {
+            console.error("Error occurred while fetching books: ", error);
+            setBooksFetched([]);
+        });
+    }
+
     return (
         <div>
-            <h1>Home</h1>
-            <Link to="/">Home</Link>
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">Close</Link>
+                    <div className="search-books-input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="Search by title, author, or ISBN"
+                        onKeyUp={(e) => searchBook(e)}
+                    />
+                    </div>
+                </div>
+            </div>
+            <div className="search-books-results">
+                <ol className="books-grid">
+                    <li>
+                        {booksFetched.map((book) => (
+                            <Book 
+                                key={book.id}
+                                book={book}
+                                onMoveBook={moveBook}
+                            />
+                        ))}
+                    </li>
+                </ol>
+            </div>    
         </div>
     );
 }
